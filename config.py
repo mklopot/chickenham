e913bb945a35c919bea6019247a4f7508d4f329d
Michael Klopot <michael.klopot@gmail.com>
2019-06-17 22:07:54 -0600

    Manage configuration file

---
+++ config.py
@@ -0,0 +1,35 @@
+import yaml
+import Path
+
+conffile = "~/.sellchiknham"
+
+
+class Data:
+    pass
+
+
+class Config:
+    def __init__(self):
+        self.keys = ["coinbase_api_key", "coinbase_api_secret"]
+        self.data = Data()
+        if Path.is_file(conffile):
+            config_on_disk = []
+            with open(conffile, 'r') as f:
+                try:
+                    config_on_disk = yaml.load(f)
+                except Exception:
+                    pass
+            for key in self.keys:
+                if key in config_on_disk:
+                    setattr(self.data, key, config_on_disk[key])
+        else:
+            for key in self.keys:
+                setattr(self.data, key, None)
+
+    def set(self, attr, value):
+        setattr(self.data, attr, value)
+        self.save()
+
+    def save(self):
+        with open(conffile, 'w') as f:
+            yaml.dump(self.data, f)
