1c0cf2fbe19f70b2a87cf77ed7b069747ce05d11
Michael Klopot <michael.klopot@gmail.com>
2019-07-07 23:49:08 -0600

    Refactor user interaction to separate function

--- coinbase_utils.py
+++ coinbase_utils.py
@@ -1,4 +1,5 @@
 from coinbase.wallet.client import Client
+import collections
 import config
 
 class Client:
@@ -28,7 +29,7 @@ class Client:
         while not client:
             coinbase_api_key = input('Enter API Key: ')
             coinbase_api_secret = input('Enter API Secret: ')
-            print(chr(27) + "[2J") #Clear Screen
+            print(chr(27) + "[2J" + chr(27) + "[H") #Clear Screen
             try:
                 client = Client(coinbase_api_key, coinbase_api_secret)
                 coinbase_user = client.get_current_user()
@@ -55,3 +56,62 @@ class Client:
         if not client:
             client = new_from_prompt(conf)
         return client
+
+def get_accounts_by_currency(c, currency='BTC'):
+    acounts = []
+        all_accounts = c.get_accounts()
+        accounts = [x for x in all_accounts.data if x.currency == currency]
+    while not accounts:
+        print("No {} accounts are showing. ".format(currency))
+        input()
+        all_accounts = c.get_accounts()
+        accounts = [x for x in all_accounts.data if x.currency == 'BTC']
+    return accounts
+
+def user_choose_confirm(client, currency="BTC", desc="account"):
+    account = None
+    while not account:
+        accounts = get_accounts_by_currency(client, currency)
+        if not accounts:
+            print("No {}s on https://coinbase.com are visible with the API Key provided. Check permissions?"
+            input("Log on to https://coinbase.com, set up the necessary account, or change API Key permissions,\n"
+                  "and press ENTER to continue...")
+            continue
+        if len(accounts) == 1:
+            print("One {} found, called '{}', with a balance of {} {}.".format(
+                      desc,
+                      accounts[0].name,
+                      accounts[0].balance.amount,
+                      accounts[0].balance.amount.currency))
+            print("This looks good. Press ENTER to confirm.\n\n"
+                  "If you have a very specific reason, and this is not the account you want to use, "
+                  "you will have to set another one up on https://coinbase.com. In that case, "
+                  "type 'no' and press ENTER once another account is set up, "
+                  "and you will have the option to select it\n"
+                  "To use this account, press ENTER")
+            user_input = input("Confirm: ")
+            user_input = user_input.to_lower()
+            if user_input[:2] != "no":
+                return accounts[0]
+        else:
+            accounts_hash = collections.ordereddict()
+            for i, account in zip(range(1,len(btc_accounts)+1),btc_accounts):
+                accounts_hash[i] = account
+
+            for account_index, account in accounts_hash.items():
+                print("{} - '{}', with a balance of {} {}\n\n".format(
+                          account_index,
+                          account.name,
+                          account.balance.amount,
+                          account.balance.currency))
+
+            print("{} - None of the above: You will need to set up another "
+                  "{} on https://coinbase.com, then select this option".format(
+                       len(accounts_hash), desc))
+            user_prompt = input("Select an option (1-{}) and press ENTER: ".format(
+                                     len(accounts_hash)))
+            try:
+                account = account_hash[int(user_prompt)]
+            except IndexError:
+                pass
+
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from coinbase.wallet.client import Client
 from pycoin.symbols.btc import network
 import config
 import collections
@@ -6,76 +5,16 @@ import getpass
 import re
 import combine
 import input_shares
+import coinbase_utils
 
-my_conf = config.Config()
-while True:
-    while not my_conf.data.coinbase_api_key:
-        coinbase_api_key = ""
-        while not coinbase_api_key or not coinbase_api_secret:
-            coinbase_api_key = input('Enter API Key: ')
-            coinbase_api_secret = input('Enter API Secret: ')
-        try:
-            c = Client(coinbase_api_key, coinbase_api_secret)
-        except Exception:
-            coinbase_api_key = None
-            coinbase_api_secret = None
-            print("The key or secret you entered were not valid. Try again...")
-            continue
-    coinbase_user = c.get_current_user()
-    print("https://coinbase.com reports the API key you entered is associated with {} <{}> residing in {}. Is this correct?".format(
-               coinbase_user.name, coinbase_user.email, coinbase_user.country.name))
-    user_prompt = input("Type 'yes' and press ENTER to confirm. Type 'no' and press ENTER to provide a different API key: ")
-    user_prompt = user_prompt.to_lower()
-    if user_prompt[:3] == "yes":
-        break
-    else:
-        print("You have chosen to use a different API key")
+conf = config.Config()
+c = coinbase_utils.Client.new(conf)
+btc_account = coinbase_utils.user_choose_confirm(c, 'BTC', 'Bitcoin account')
+deposit_address = account.create_address().address    
+usd_account = coinbase_utils.user_choose_confirm(c, "USD", 'USD account')
 
-my_conf.set('coinbase_api_key', coinbase_api_key)
-my_conf.set('coinbase_api_secret', coinbase_api_secret)   
-
-def get_accounts_by_currency(c, currency='BTC'):
-    acounts = []
-        all_accounts = c.get_accounts()
-        accounts = [x for x in all_accounts.data if x.currency == currency]
-    while not accounts:
-        print("No {} accounts are showing. ".format(currency))
-        input()
-        all_accounts = c.get_accounts()
-        accounts = [x for x in all_accounts.data if x.currency == 'BTC']
-    return accounts
-
-btc_accounts = get_accounts_by_currency(c)
-if len(btc_accounts) == 1:
-    print("One Bitcoin account found, called '{}', with a balance of {} {}.".format(
-               btc_accounts[0].name, btc_accounts[0].balance.amount, btc_accounts[0].balance.amount.currency))
-    print("This looks good. Press ENTER to confirm.\n\n"
-          "If you have a very specific reason, and this is not the account you want to use, "
-          "you will have to set another one up on https://coinbase.com. In that case, "
-          "type 'no' and press ENTER once another account is set up, and you will have the option to select it"
-          "To use this acocunt, press ENTER")
-    user_prompt = input("Confirm: ")
-    user_prompt = user_prompt.to_lower()
-    # TODO: query accounts again here
-    btc_account = btc_accounts[0]
-else:
-    accounts_hash = collections.ordereddict()
-    for i, account in zip(range(1,len(btc_accounts)+1),btc_accounts):
-        accounts_hash[i] = account
-
-    for account_index, account in accounts_hash.items():
-        print("{} - '{}', with a balance of {} {}\n\n".format(account_index, account.name, account.balance.amount, account.balance.currency))
-    print("{} - None of the above: You will need to set up another "
-          "Bitcoin account on https://coinbase.com, then select this option".format(len(accounts_hash)))
-    user_prompt = input("Select an option (1-{}) and press ENTER: ".format(len(accounts_hash)))
-    btc_account = account_hash[int(user_prompt)]
-    
-address_object = account.create_address()    
-deposit_address = address_object.address
-
-usd_account = get_accounts_by_currency(c, "USD")[0]
-
-while True:
+secret = None
+while not secret:
     shares = input_shares.input_batch()
     combiner = combine.Combiner(len(shares))
     secret = combiner(shares)
@@ -83,7 +22,6 @@ while True:
         print("The Shared Codes could not be combined. Try again. Starting over..."
     else:
         print("Shared Codes successfully combined!")
-        break
 
 private_key = network.keys.private(secret_exponent=secret)
 wif = private_key.wif()
