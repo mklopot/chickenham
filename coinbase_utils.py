4d871af740b2c37f5c52019c044196c8623808ed
Michael Klopot <michael.klopot@gmail.com>
2019-07-24 00:20:47 -0600

    Selecting payment method from multiple options

--- coinbase_utils.py
+++ coinbase_utils.py
@@ -19,7 +19,7 @@ class CoinClient:
                     print("Email: " + colored(coinbase_user.email, "blue"))
                     print("Country: " + colored(coinbase_user.country.name, "blue"))
                     print("Do you want to proceed using this account?")
-                    print(colored("Type ", "cyan") + colored("yes", "yellow") + 
+                    print(colored("Type ", "cyan") + colored("yes", "yellow") +
                           colored(" and press ENTER to confirm.\n", "cyan") +
                           colored("Type ", "cyan") + colored("no", "yellow") +
                           colored(" and press ENTER to be prompted "
@@ -28,7 +28,7 @@ class CoinClient:
                     user_prompt = user_prompt.lower()
                     if user_prompt[:1] == "y":
                         return client
-                except Exceptionas e:
+                except Exception as e:
                     print(colored("The previously stored API Key or API Secret is invalid", "red"))
                     print(colored(e, "cyan"))
                     delete_api_key = input("Delete stored API key and API Secret? (yes/no): ")
@@ -55,7 +55,7 @@ class CoinClient:
                 print("Email: " + colored(coinbase_user.email, "blue"))
                 print("Country: " + colored(coinbase_user.country.name, "blue"))
                 print("Do you want to proceed using this account?")
-                print(colored("Type ", "cyan") + colored("yes", "yellow") + 
+                print(colored("Type ", "cyan") + colored("yes", "yellow") +
                       colored(" and press ENTER to confirm.\n", "cyan") +
                       colored("Type ", "cyan") + colored("no", "yellow") +
                       colored(" and press ENTER to be prompted "
@@ -67,7 +67,7 @@ class CoinClient:
                     conf.set('coinbase_api_secret', coinbase_api_secret)
                     return client
                 else:
-                    print(colored("Discaring the API KEY and API Secret "
+                    print(colored("Discarding the API KEY and API Secret "
                                   "you entered, starting over...", "red"))
             except Exception as e:
                 print(colored("The API Key or API Secret you entered is invalid", "red"))
@@ -86,7 +86,7 @@ def get_accounts_by_currency(c, currency='BTC'):
     all_accounts = c.get_accounts()
     accounts = [x for x in all_accounts.data if x.currency == currency]
     while not accounts:
-        print(colored("No {} accounts are showing".format(currency)), "red)
+        print(colored("No {} account retrieved".format(currency), "red"))
         print(colored("Log on to https://coinbase.com and check API key permissions.", "cyan"))
         input("Press ENTER to retry ")
         all_accounts = c.get_accounts()
@@ -95,100 +95,94 @@ def get_accounts_by_currency(c, currency='BTC'):
 
 
 def user_choose_confirm(client, currency="BTC", desc="account"):
-    account = None
-    while not account:
+    sel_account = None
+    while not sel_account:
         accounts = get_accounts_by_currency(client, currency)
         if not accounts:
-            print("No {}s on https://coinbase.com are visible with the API Key provided.\n"
-                  "Check permissions...")
-            input("Log on to https://coinbase.com, set up the necessary account,\n"
-                  "or change API Key permissions.\n"
-                  "Then press ENTER to continue...")
             continue
         if len(accounts) == 1:
-            print("One {} found, called '{}', with a balance of {} {}.".format(
-                      desc,
-                      accounts[0].name,
-                      accounts[0].balance.amount,
-                      accounts[0].balance.currency))
-            print("This looks good. Press ENTER to confirm.\n\n"
-                  "If you have a very specific reason,\n"
-                  "and this is not the account you want to use, "
-                  "you will have to set another one up on https://coinbase.com. In that case, "
-                  "type 'no' and press ENTER once another account is set up, "
-                  "and you will have the option to select it\n"
-                  "To use this account, press ENTER")
-            user_input = input("Confirm: ")
+            print("One {} {} found:".format(currency, desc))
+            print("Name: " + colored("{}".format(accounts[0].name), "blue"))
+            print("Balance: " + colored("{} {}".format(accounts[0].balance.amount,
+                                                       accounts[0].balance.currency)))
+            print("To use this account, press ENTER")
+            print(colored("If this is not the account you want to use,\n"
+                          "you can set up another {} account "
+                          "on https://coinbase.com\n".format(currency), "cyan"))
+            print(colored("In that case, type ", "cyan") + colored("no", "yellow") +
+                  colored(" and press ENTER once another account is set up, "
+                          "and you will have the option to select it.\n", "cyan"))
+            user_input = input("Use this account (Y/n): ")
             user_input = user_input.lower()
-            if user_input[:2] != "no":
+            if user_input[:1] != "n":
                 return accounts[0]
         else:
-            accounts_hash = collections.ordereddict()
+            accounts_hash = collections.OrderedDict()
             for i, account in zip(range(1, len(accounts)+1), accounts):
                 accounts_hash[i] = account
 
             for account_index, account in accounts_hash.items():
-                print("{} - '{}', with a balance of {} {}\n\n".format(
-                          account_index,
-                          account.name,
-                          account.balance.amount,
-                          account.balance.currency))
+                print("{} - ".format(account_index) +
+                      colored("{} {}\n".format(
+                                  account.balance.amount,
+                                  account.balance.currency),
+                              "blue"))
 
-            print("{} - None of the above: You will need to set up another "
-                  "{} on https://coinbase.com, then select this option".format(
-                       len(accounts_hash), desc))
-            user_prompt = input("Select an option (1-{}) and press ENTER: ".format(
+            print("{} - None of the above\n".format(len(accounts_hash)) +
+                  colored("        You can set up another {} {} on\n"
+                          "        https://coinbase.com, then select this option.".format(
+                              currency, desc),
+                          "cyan"))
+            user_prompt = input("\nSelect an option (1-{}) and press ENTER: ".format(
                                      len(accounts_hash)))
             try:
-                account = accounts_hash[int(user_prompt)]
-            except IndexError:
-                pass
+                sel_account = accounts_hash[int(user_prompt)]
+                return sel_account
+            except (KeyError, ValueError):
+                continue
 
 
 def user_choose_payment_method(c):
-    method = None
-    while not method:
+    sel_method = None
+    while not sel_method:
         payment_methods = c.get_payment_methods()
         methods = [p for p in payment_methods.data if p.allow_withdraw and p.currency == 'USD']
         if not methods:
-            print("No accounts linked for withdrawal are visible on https://coinbase.com\n"
-                  "with the API Key provided.\n"
-                  "Check permissions...")
+            print(colored("No linked USD withdrawal accounts could be retrieved"
+                  "with the API Key provided", "red"))
             input("Log on to https://coinbase.com, set up the necessary account,\n"
                   "or change API Key permissions.\n"
                   "Then press ENTER to continue...")
             continue
         if len(methods) == 1:
-            print("One {} withdrawal method found: {}".format(
-                      methods[0].currency,
-                      methods[0].name))
-            print("This looks good. Press ENTER to confirm.\n\n"
-                  "If this is not the account you want to use, "
-                  "you will have to set another one up on https://coinbase.com. In that case, "
-                  "type 'no' and press ENTER once another account is set up, "
-                  "and you will have the option to select it\n"
-                  "To use this account, press ENTER")
-            user_input = input("Confirm: ")
+            print("\nOne linked account found: " + colored("{}".format(
+                      methods[0].name), "blue"))
+            print("To use this account, press ENTER")
+            print(colored("If this is not the account you want to use, you can set up another\n"
+                          "linked USD withdrawal account up on https://coinbase.com", "cyan"))
+            print(colored("In that case, type ", "cyan") + colored("no", "yellow") +
+                  colored(" and press ENTER once another account is set up,\n"
+                          "and you will have the option to select it.", "cyan"))
+            user_input = input("Use this account (Y/n): ")
             user_input = user_input.lower()
-            if user_input[:2] != "no":
+            if user_input[:1] != "n":
                 return methods[0]
         else:
-            methods_hash = collections.ordereddict()
+            print("\nSelect a linked USD withdrawal account:")
+            methods_hash = collections.OrderedDict()
             for i, method in zip(range(1, len(methods)+1), methods):
                 methods_hash[i] = method
 
             for method_index, method in methods_hash.items():
-                print("{} - '{}', currency: {}\n\n".format(
-                          method_index,
-                          method.name,
-                          method.currency))
+                print("{} - ".format(method_index) + colored("{}\n".format(method.name), "blue"))
+            print("{} - None of the above\n".format(len(methods_hash)+1) +
+                  colored("     You can set up another linked USD withdrawal method on\n"
+                          "     https://coinbase.com, then select this option.", "cyan"))
 
-            print("{} - None of the above: You will need to set up another "
-                  "withdrawal method on https://coinbase.com, then select this option".format(
-                       len(methods_hash)))
-            user_prompt = input("Select an option (1-{}) and press ENTER: ".format(
-                                     len(methods_hash)))
+            user_prompt = input("\nSelect an option (1-{}) and press ENTER: ".format(
+                                     len(methods_hash)+1))
             try:
-                method = methods_hash[int(user_prompt)]
-            except IndexError:
-                pass
+                sel_method = methods_hash[int(user_prompt)]
+                return sel_method
+            except (KeyError, ValueError):
+                continue
